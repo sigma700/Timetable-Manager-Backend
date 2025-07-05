68327b7bdde3951061ebeb8789f162e307f18e39
Sigma700 <allankirimi65@gmail.com>
2025-07-05 13:59:12 +0300

    added the route

--- routes/dataRouter.js
+++ routes/dataRouter.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { listSubjects, listTeachers } from '../controllers/adminController.js';
+import { listClassData, listSubjects, listTeachers } from '../controllers/adminController.js';
 
 const dataRouter = Router();
 
 dataRouter.post('/list-teachers', listTeachers);
 dataRouter.post('/list-subjects', listSubjects);
+dataRouter.post('/list-classData', listClassData);
 
 export { dataRouter };
