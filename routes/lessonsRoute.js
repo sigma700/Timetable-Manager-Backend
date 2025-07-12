7efedebb72b3349dba132dc6dfd1a3e050396d57
Sigma700 <allankirimi65@gmail.com>
2025-07-12 23:23:46 +0300

    removed the folder from the root and placed it into the src folder

--- routes/lessonsRoute.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-import { createLesson } from '../controllers/lessonsController.js';
-
-export const lessonRouter = Router();
-
-lessonRouter.post('/checkOverlap', createLesson);
