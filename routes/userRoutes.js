bc0c416dee659c4025600d1f46d76e95ded6b384
Sigma700 <allankirimi65@gmail.com>
2025-07-12 20:52:33 +0300

    added the middleware to check the userAuthentication

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import { createTeacher, veriAcc } from '../controllers/userController.js';
 
+import { checkAuth } from '../controllers/middlewareController.js';
+
 export const router = Router();
 
-router.post('/create-account', createTeacher);
+router.get('check', checkAuth);
+router.post('/create-account', checkAuth, createTeacher);
 router.post('/verify', veriAcc);
