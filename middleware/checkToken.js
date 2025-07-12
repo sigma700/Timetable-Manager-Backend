f1328a49c6d43b32a41927563733a1caff60876e
Sigma700 <allankirimi65@gmail.com>
2025-07-12 21:04:29 +0300

    added the token cookie authenticator as the middleware pattern

---
+++ middleware/checkToken.js
@@ -0,0 +1,28 @@
+//checking users decoded cookie
+
+import { sendError, sendSucess } from '../utils/sendError.js';
+import jwt from 'jsonwebtoken';
+export const checkAuthentication = async (req, res, next) => {
+	const { token } = req.cookies;
+
+	if (!token) {
+		sendError(res, 'Are you logged in yet ?', 401);
+		return;
+	}
+
+	try {
+		const decodedToken = jwt.verify(token, process.env.WEBTOKEN);
+		if (!decodedToken) {
+			sendError(res, 'Failed could not decode the token !', 500);
+		}
+
+		sendSucess(res, 'Sucess !', decodedToken, 200);
+
+		req.userId = decodedToken.userId;
+		next(); //gives the logic to any of the related functions
+	} catch (error) {
+		console.log(error);
+
+		sendError(res, error.message);
+	}
+};
