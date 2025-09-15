1cd86396410312f6366b1d9e84b0952c41ca91f9
Sigma700 <allankirimi65@gmail.com>
2025-09-15 23:16:40 +0300

    fixed the api routes so as to make the code run again

--- src/routes/dataRouter.js
+++ src/routes/dataRouter.js
@@ -13,14 +13,15 @@ import { verifyToken } from '../../middleware/checkToken.js';
 
 const dataRouter = Router();
 
-dataRouter.post('/list-teachers/:schoolId', listTeachers);
-dataRouter.post('/list-subjects/:schoolId', listSubjects);
-dataRouter.post('/list-classData/:schoolId', listClassData);
-dataRouter.post('/gen-table/:schoolId', verifyToken, genTimetableHandler);
+dataRouter.post('/list-school', verifyToken, listSchool);
+dataRouter.post('/list-subjects', verifyToken, listSubjects);
+dataRouter.post('/list-classData', verifyToken, listClassData);
+dataRouter.post('/list-teachers', verifyToken, listTeachers);
 dataRouter.put('/updateTable/:timetableId', updateTimetable);
 dataRouter.delete('/delTable/:timetableId', deleteTable);
 //now to get all the data about the timetable that has been gerenrated !
 dataRouter.get('/getTable/:timetableId', verifyToken, getTimetable);
-dataRouter.post('/list-school', listSchool);
+dataRouter.post('/gen-table', verifyToken, genTimetableHandler);
+// dataRouter.post('/list-school', listSchool);
 
 export { dataRouter };
