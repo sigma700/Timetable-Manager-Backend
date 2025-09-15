4f398e02635ae69e8bc687e6a9cacd6e0fbab860
Sigma700 <allankirimi65@gmail.com>
2025-09-15 23:18:08 +0300

    fixed the user routes to make the code work as well as required

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,7 +5,7 @@ import { checkAuth } from '../controllers/middlewareController.js';
 import { verifyToken } from '../../middleware/checkToken.js';
 
 export const router = Router();
-router.post('/create-account/:school', createTeacher);
+router.post('/create-account', createTeacher);
 router.post('/login/:school', login);
 router.post('/verify', veriAcc);
 router.get('/check-Auth', verifyToken, checkAuth);
