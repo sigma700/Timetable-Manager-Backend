6aa921fa466b60ae340d5e8312d6f94c43fb1e0d
Sigma700 <allankirimi65@gmail.com>
2025-09-17 23:45:49 +0300

    final commit

--- src/database/model/school.js
+++ src/database/model/school.js
@@ -5,9 +5,8 @@ const schoolSchema = new Schema({
 	name: {
 		type: String,
 		required: true,
-		unique: true,
 	},
-	address: String,  //this is the main and the main area of thinking and should not be able to make sure that the same person is not the main character in the story and should make everything work out for u
+	address: String, //this is the main and the main area of thinking and should not be able to make sure that the same person is not the main character in the story and should make everything work out for u
 	contactEmail: String,
 	phoneNumber: String,
 });
