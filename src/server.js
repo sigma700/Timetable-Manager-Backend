b4bf3c840a3944a32e97ad882e19bc06b75c77aa
Sigma700 <allankirimi65@gmail.com>
2025-09-18 00:06:54 +0300

    final commit

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: 'http://localhost:5173',
+		origin: 'https://protiba.onrender.com',
 		methods: ['GET , POST , PUT , DELETE , PATCH'],
 		credentials: true,
 	})
