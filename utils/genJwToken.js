65e689c23ec6a514988c0a94bfa131ebfa6fbd5b
sigma700 <196065961+sigma700@users.noreply.github.com>
2026-08-14 04:35:49 +0300

    done some changes for deploying

--- utils/genJwToken.js
+++ utils/genJwToken.js
@@ -7,20 +7,20 @@ export const genJwTok = (res, userId) => {
 
   const isProduction = process.env.NODE_ENV === "production";
   // DEV SETTINGS
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: isProduction,
-    sameSite: isProduction ? "none" : "lax",
-  });
-
-  //PRODUCTION SETTINGS
   // res.cookie("token", token, {
   //   httpOnly: true,
   //   secure: isProduction,
-  //   sameSite: isProduction ? "strict" : "lax",
-  //   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
-  //   path: "/",
+  //   sameSite: isProduction ? "none" : "lax",
   // });
+
+  //PRODUCTION SETTINGS
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction ? "strict" : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+    path: "/",
+  });
 
   return token;
 };
