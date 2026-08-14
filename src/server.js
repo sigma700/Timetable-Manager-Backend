6fda8f1874dd60dffd994177c5d05cbeac6850e7
sigma700 <196065961+sigma700@users.noreply.github.com>
2026-08-14 04:57:39 +0300

    Final commit of the day

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: " https://protiba.onrender.com/",
+    origin: " https://protiba.onrender.com",
     methods: ["GET , POST , PUT , DELETE , PATCH"],
     credentials: true,
   }),
